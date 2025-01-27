148ceb2d06cccf85c92932bc29539cddb1a775b9
Fish <fishagbalo@gmail.com>
2025-01-27 23:02:33 +0800

    Add Posts and Sessions controllers; implement routes for signup and login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,5 @@
+class SessionsController < ApplicationController
+  def create
+    raise "LOGIN"
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,6 @@
 class UsersController < ApplicationController
   def create
-    user = User.new(user_params)
-    if user.save
-      render json: { token: user.generate_jwt }, status: :created
-    else
-      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
-    end
+   raise "SIGNUP"
   end
 
   private
